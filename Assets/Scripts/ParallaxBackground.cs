c5089c33b9c34e14946308cb63a7145d0e5d30d5
AliRahnama <AliRahnama.git@gmail.com>
2025-05-31 04:15:13 +0330

    levelDesign

--- Assets/Scripts/ParallaxBackground.cs
+++ Assets/Scripts/ParallaxBackground.cs
@@ -4,19 +4,33 @@ public class ParallaxBackground : MonoBehaviour
 {
     public Transform cameraTransform;
     public float parallaxFactor = 0.5f;
-    private Vector3 previousCameraPosition;
+    public float activateDistance = 30f;
+
+    private Vector3 startPosition;
+    private Vector3 startCameraPosition;
+    private bool initialized = false;
 
     void Start()
     {
-        previousCameraPosition = cameraTransform.position;
+        if (cameraTransform == null)
+            cameraTransform = Camera.main.transform;
     }
 
     void LateUpdate()
     {
-        Vector3 deltaMovement = cameraTransform.position - previousCameraPosition;
-        
-        transform.position += new Vector3(deltaMovement.x * parallaxFactor, 0f, 0f);
+        if (!initialized)
+        {
+            float distance = Vector3.Distance(transform.position, cameraTransform.position);
+            if (distance < activateDistance)
+            {
+                startPosition = transform.position;
+                startCameraPosition = cameraTransform.position;
+                initialized = true;
+            }
+            else return;
+        }
 
-        previousCameraPosition = cameraTransform.position;
+        Vector3 delta = cameraTransform.position - startCameraPosition;
+        transform.position = startPosition + new Vector3(delta.x * parallaxFactor, 0f, 0f);
     }
 }
